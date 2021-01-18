aecfe4be6692f35c5b3e29122d0f483d125c71ab
dinhconghuan <62755292+dinhconghuan@users.noreply.github.com>
2021-01-19 01:43:03 +0700

    first commit

---
+++ helloword/index.php
@@ -0,0 +1,21 @@
+
+<html>
+
+<head>
+    <title>Hello Word</title>
+</head>
+
+<body>
+    <div>
+        <center>
+            <h1>Hello word</h1>
+            <form action="/helloword/index2.php">
+                <label for="name">Vui lòng nhập tên người dùng:</label><br><br>
+                <input type="text" id="fname" name="fname"><br><br>
+                <input type="submit">
+            </form>
+        </center>
+    </div>
+</body>
+
+</html>
---
+++ helloword/index2.php
@@ -0,0 +1,17 @@
+
+<html>
+
+<head>
+    <title>Hello Word</title>
+</head>
+
+<body>
+    <div>
+        <center>
+            <h1>Hello word</h1>
+        <?php echo "Chào mừng " , "fname " , "đã đến với môn học LTQL" ?>
+        </center>
+    </div>
+</body>
+
+</html>
